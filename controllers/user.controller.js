794ccacf618c269de4311dd9d6394cf035b6fb30
Taha <taha.iu.bd.com>
2024-05-11 21:45:34 +0600

    all user get

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,7 +1,17 @@
 const users = require('../public/user.json')
 
+// Random user from json
 module.exports.getRandomUser = (req, res) => {
   const randomIndex = Math.floor(Math.random() * 20)
   const randomUser = users[randomIndex]
-  res.status(200).send(randomUser)
+  res.status(200).send({ success: true, message: 'success', data: randomUser })
+}
+
+// All users from json
+module.exports.getAllUsers = (req, res) => {
+  res.status(200).send({
+    success: true,
+    message: 'success',
+    data: users,
+  })
 }
--- routes/users.routers.js
+++ routes/users.routers.js
@@ -1,9 +1,10 @@
-const { getRandomUser } = require('../controllers/user.controller')
+const userControllers = require('../controllers/user.controller')
 
 const express = require('express')
 
 const router = express.Router()
 
-router.route('/random').get(getRandomUser)
+router.route('/random').get(userControllers.getRandomUser)
+router.route('/all').get(userControllers.getAllUsers)
 
 module.exports = router
