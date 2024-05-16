fb0b871e8b480a69bd03e5cbd3ba06c762eded29
Taha <taha.iu.bd.com>
2024-05-16 15:13:17 +0600

    deploy

--- util/getUsers.js
+++ util/getUsers.js
@@ -4,6 +4,7 @@ const getUsers = async () => {
   try {
     const parsedUsers = await fs.readFile('./public/user.json', 'utf8')
     const users = JSON.parse(parsedUsers)
+    console.log('hello users', users)
     return users
   } catch (err) {
     console.log(err)
