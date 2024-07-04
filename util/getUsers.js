cb42781368f76d50decd6af1de2048321d1d240c
Taha <taha.iu.bd.com>
2024-07-04 18:43:06 +0600

    rout

--- util/getUsers.js
+++ util/getUsers.js
@@ -2,7 +2,7 @@ const fs = require('fs').promises
 
 const getUsers = async () => {
   try {
-    const parsedUsers = await fs.readFile('/user.json', 'utf8')
+    const parsedUsers = await fs.readFile('./public/user.json', 'utf8')
     const users = JSON.parse(parsedUsers)
     console.log('hello users hello', users)
     return users
